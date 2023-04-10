4af980583161468f4be566886c2d4fd345c3a2f2
Daniel Modesto <danielmodesto41@gmail.com>
2023-04-10 09:13:27 -0500

    Se crea vista de procesos, estado lote y resumen lote sin navbar

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
 
-function gg(){
-    let link = document.getElementById("link").value
-    // console.log(link)
-
-    const site = document.createElement("iframe");
-    // site.src = link;
-    site.src = "https://github.com/Danielm2003/Fruandes-test"
-
-    document.body.appendChild(site);
-}
+setInterval("location.reload()",1800000);
